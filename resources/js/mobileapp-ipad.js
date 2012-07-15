7bb8e90c46642bd76d9b06fa40630b437854ef4b
Akhilesh Gupta <g_akhilesh@yahoo.co.in>
2012-07-15 13:21:37 -0700

    using https for google maps api

--- resources/js/mobileapp-ipad.js
+++ resources/js/mobileapp-ipad.js
@@ -574,7 +574,7 @@ function switchDetailSection(section, contact, callback) {
 
 function codeAddressOnMap(address) {
     var encodedAdd = encodeURI(address);
-    var mapsImage = '<img src="http://maps.googleapis.com/maps/api/staticmap?' + 
+    var mapsImage = '<img src="https://maps.googleapis.com/maps/api/staticmap?' + 
                     'zoom=14&size=640x180&format=jpeg&sensor=false&markers=color:red%7C' + 
                     encodedAdd + '"/>';
     $j('#map_section #map_div #google_map_canvas').empty().append(mapsImage);
